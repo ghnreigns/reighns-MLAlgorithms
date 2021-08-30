98086765076163b24a4b814872d96dbfe7d3d5ec
reigHns <reighns.sjr.sjh.hrj@gmail.com>
2021-08-30 13:26:47 +0800

    Working on decision trees.

--- reighns-decision-trees/decision_tree_naive.py
+++ reighns-decision-trees/decision_tree_naive.py
@@ -5,53 +5,95 @@ sys.path.append(os.getcwd())  # noqa
 from typing import *
 import importlib
 
-Entropy = importlib.import_module("reighns-utils.entropy", package="reighns-utils")
-
-
-import numpy as np
+Entropy = importlib.import_module(
+    "reighns-utils.scripts.entropy", package="reighns-utils"
+)
 
 
 def partition_entropy(subsets: List[List[Any]]) -> float:
     """Returns the entropy from this partition of data into subsets"""
     total_count = sum(len(subset) for subset in subsets)
+
     return sum(
         Entropy.calculate_entropy(subset) * len(subset) / total_count
         for subset in subsets
     )
 
 
-def info_gain(data, labels, attribute, attributes):
-    """
-    Args:
-      data(m, D): The current sub-dataset of the sub-tree
-          m: num_rows
-          D: num_features
-      labels(m): The corresponding labels of current sub-dataset of the sub-tree
-          m: num_rows
-      attribute: The attribute used to split data
-      attributes: The list of current remaining attributes
-    Returns:
-      info_gain : information gain of the given dataset
-    """
+import numpy as np
 
-    freq = {}
-    subsetEntropy = 0.0
+from typing import NamedTuple, Optional
 
-    # Get the column index of this attribute
-    i = attributes.index(attribute)
 
-    for entry in data:
-        if entry[i] in freq:
-            freq[entry[i]] += 1.0
-        else:
-            freq[entry[i]] = 1.0
+class Candidate(NamedTuple):
+    level: str
+    lang: str
+    tweets: bool
+    phd: bool
+    did_well: Optional[bool] = None  # allow unlabeled data
 
-    ###########################
-    #
-    # Your Turn (Q6): Write your code here
-    # Hint: Split the data based on the value at index i. Find the subsetEntropy of
-    # each sub-dataset, then use the formula of Information Gain to calculate subsetEntropy.
-    #
 
-    ###########################
-    return entropy(labels) - subsetEntropy
+#  level     lang     tweets  phd  did_well
+#  Senior    Python   False   True  True
+
+inputs = [
+    Candidate("Senior", "Java", False, False, False),
+    Candidate("Senior", "Java", False, True, False),
+    Candidate("Mid", "Python", False, False, True),
+    Candidate("Junior", "Python", False, False, True),
+    Candidate("Junior", "R", True, False, True),
+    Candidate("Junior", "R", True, True, False),
+    Candidate("Mid", "R", True, True, True),
+    Candidate("Senior", "Python", False, False, False),
+    Candidate("Senior", "R", True, False, True),
+    Candidate("Junior", "Python", True, False, True),
+    Candidate("Senior", "Python", True, True, True),
+    Candidate("Mid", "Python", False, True, True),
+    Candidate("Mid", "Java", True, False, True),
+    Candidate("Junior", "Python", False, True, False),
+]
+
+from typing import Dict, TypeVar
+from collections import defaultdict
+
+T = TypeVar("T")  # generic type for inputs
+
+
+def partition_by(inputs: List[T], attribute: str) -> Dict[Any, List[T]]:
+    """Partition the inputs into lists based on the specified attribute."""
+    partitions: Dict[Any, List[T]] = defaultdict(list)
+    for input in inputs:
+        key = getattr(input, attribute)  # value of the specified attribute
+        partitions[key].append(input)  # add input to the correct partition
+    return partitions
+
+
+def partition_entropy_by(
+    inputs: List[Any], attribute: str, label_attribute: str
+) -> float:
+    """Compute the entropy corresponding to the given partition"""
+    # partitions consist of our inputs
+    partitions = partition_by(inputs, attribute)
+
+    # but partition_entropy needs just the class labels
+    labels = [
+        [getattr(input, label_attribute) for input in partition]
+        for partition in partitions.values()
+    ]
+
+    return partition_entropy(labels)
+
+
+for key in ["level", "lang", "tweets", "phd"]:
+    print(key, partition_entropy_by(inputs, key, "did_well"))
+
+assert 0.69 < partition_entropy_by(inputs, "level", "did_well") < 0.70
+assert 0.86 < partition_entropy_by(inputs, "lang", "did_well") < 0.87
+assert 0.78 < partition_entropy_by(inputs, "tweets", "did_well") < 0.79
+assert 0.89 < partition_entropy_by(inputs, "phd", "did_well") < 0.90
+
+senior_inputs = [input for input in inputs if input.level == "Senior"]
+
+assert 0.4 == partition_entropy_by(senior_inputs, "lang", "did_well")
+assert 0.0 == partition_entropy_by(senior_inputs, "tweets", "did_well")
+assert 0.95 < partition_entropy_by(senior_inputs, "phd", "did_well") < 0.96
